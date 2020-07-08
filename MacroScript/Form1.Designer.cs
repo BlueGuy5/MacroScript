c9c62ef43aa31fb210cc8b62f4e896a3b636e417
BlueGuy5 <55474089+BlueGuy5@users.noreply.github.com>
2020-07-08 16:59:45 -0700

    v1.0

--- MacroScript/Form1.Designer.cs
+++ MacroScript/Form1.Designer.cs
@@ -35,6 +35,11 @@
             this.txt_ProcessName = new System.Windows.Forms.TextBox();
             this.list_ProcessName = new System.Windows.Forms.ListBox();
             this.list_txtFiles = new System.Windows.Forms.ListBox();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.pic_PlayButton = new System.Windows.Forms.PictureBox();
+            this.pic_reload = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_PlayButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_reload)).BeginInit();
             this.SuspendLayout();
             // 
             // Panel_readfile
@@ -45,18 +50,19 @@
             this.Panel_readfile.AutoScroll = true;
             this.Panel_readfile.Location = new System.Drawing.Point(238, 64);
             this.Panel_readfile.Name = "Panel_readfile";
-            this.Panel_readfile.Size = new System.Drawing.Size(228, 170);
+            this.Panel_readfile.Size = new System.Drawing.Size(228, 270);
             this.Panel_readfile.TabIndex = 1;
             // 
             // txt_readfiles
             // 
-            this.txt_readfiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_readfiles.Location = new System.Drawing.Point(238, 240);
+            this.txt_readfiles.AllowDrop = true;
+            this.txt_readfiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt_readfiles.Location = new System.Drawing.Point(0, 192);
             this.txt_readfiles.Multiline = true;
             this.txt_readfiles.Name = "txt_readfiles";
             this.txt_readfiles.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txt_readfiles.Size = new System.Drawing.Size(228, 99);
+            this.txt_readfiles.Size = new System.Drawing.Size(232, 115);
             this.txt_readfiles.TabIndex = 2;
             // 
             // txt_DirFiles
@@ -90,20 +96,57 @@
             // 
             // list_txtFiles
             // 
-            this.list_txtFiles.Dock = System.Windows.Forms.DockStyle.Left;
             this.list_txtFiles.FormattingEnabled = true;
             this.list_txtFiles.Location = new System.Drawing.Point(0, 0);
             this.list_txtFiles.Name = "list_txtFiles";
-            this.list_txtFiles.Size = new System.Drawing.Size(232, 339);
+            this.list_txtFiles.Size = new System.Drawing.Size(232, 186);
             this.list_txtFiles.Sorted = true;
             this.list_txtFiles.TabIndex = 7;
             this.list_txtFiles.SelectedIndexChanged += new System.EventHandler(this.list_txtFiles_SelectedIndexChanged);
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Open.Location = new System.Drawing.Point(48, 313);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(62, 20);
+            this.btn_Open.TabIndex = 8;
+            this.btn_Open.Text = "Open";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Browse_Click);
+            // 
+            // pic_PlayButton
+            // 
+            this.pic_PlayButton.BackgroundImage = global::MacroScript.Properties.Resources.playbutton;
+            this.pic_PlayButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pic_PlayButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_PlayButton.Location = new System.Drawing.Point(12, 310);
+            this.pic_PlayButton.Name = "pic_PlayButton";
+            this.pic_PlayButton.Size = new System.Drawing.Size(30, 21);
+            this.pic_PlayButton.TabIndex = 12;
+            this.pic_PlayButton.TabStop = false;
+            this.pic_PlayButton.Click += new System.EventHandler(this.pic_PlayButton_Click);
+            // 
+            // pic_reload
+            // 
+            this.pic_reload.BackgroundImage = global::MacroScript.Properties.Resources.macros_512;
+            this.pic_reload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pic_reload.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_reload.Location = new System.Drawing.Point(116, 313);
+            this.pic_reload.Name = "pic_reload";
+            this.pic_reload.Size = new System.Drawing.Size(28, 18);
+            this.pic_reload.TabIndex = 11;
+            this.pic_reload.TabStop = false;
+            this.pic_reload.Click += new System.EventHandler(this.pic_Reload_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(712, 339);
+            this.Controls.Add(this.pic_PlayButton);
+            this.Controls.Add(this.pic_reload);
+            this.Controls.Add(this.btn_Open);
             this.Controls.Add(this.list_txtFiles);
             this.Controls.Add(this.list_ProcessName);
             this.Controls.Add(this.txt_ProcessName);
@@ -115,6 +158,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MacroScript";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_PlayButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_reload)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,6 +172,9 @@
         private System.Windows.Forms.TextBox txt_ProcessName;
         private System.Windows.Forms.ListBox list_ProcessName;
         private System.Windows.Forms.ListBox list_txtFiles;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.PictureBox pic_reload;
+        private System.Windows.Forms.PictureBox pic_PlayButton;
     }
 }
 
