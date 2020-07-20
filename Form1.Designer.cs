2785a031cf89e3b6511e2cb221b3abc49b121a59
BlueGuy5 <55474089+BlueGuy5@users.noreply.github.com>
2020-07-20 10:11:14 -0700

    v5.2
    fix fw path...reuploaded

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace MacroScript
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Panel_readfile = new System.Windows.Forms.FlowLayoutPanel();
+            this.txt_readfiles = new System.Windows.Forms.TextBox();
+            this.txt_DirFiles = new System.Windows.Forms.TextBox();
+            this.list_txtFiles = new System.Windows.Forms.ListBox();
+            this.pic_open = new System.Windows.Forms.PictureBox();
+            this.pic_PlayButton = new System.Windows.Forms.PictureBox();
+            this.pic_reload = new System.Windows.Forms.PictureBox();
+            this.DropDown_Process = new Syncfusion.Windows.Forms.Tools.ComboBoxAdv();
+            this.btn_GetFileDir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_open)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_PlayButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_reload)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DropDown_Process)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Panel_readfile
+            // 
+            this.Panel_readfile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Panel_readfile.AutoScroll = true;
+            this.Panel_readfile.Location = new System.Drawing.Point(240, 55);
+            this.Panel_readfile.Name = "Panel_readfile";
+            this.Panel_readfile.Size = new System.Drawing.Size(205, 122);
+            this.Panel_readfile.TabIndex = 1;
+            // 
+            // txt_readfiles
+            // 
+            this.txt_readfiles.AllowDrop = true;
+            this.txt_readfiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_readfiles.Location = new System.Drawing.Point(239, 183);
+            this.txt_readfiles.Multiline = true;
+            this.txt_readfiles.Name = "txt_readfiles";
+            this.txt_readfiles.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_readfiles.Size = new System.Drawing.Size(206, 148);
+            this.txt_readfiles.TabIndex = 2;
+            // 
+            // txt_DirFiles
+            // 
+            this.txt_DirFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_DirFiles.Location = new System.Drawing.Point(239, 2);
+            this.txt_DirFiles.Name = "txt_DirFiles";
+            this.txt_DirFiles.Size = new System.Drawing.Size(206, 20);
+            this.txt_DirFiles.TabIndex = 3;
+            // 
+            // list_txtFiles
+            // 
+            this.list_txtFiles.Dock = System.Windows.Forms.DockStyle.Left;
+            this.list_txtFiles.FormattingEnabled = true;
+            this.list_txtFiles.Location = new System.Drawing.Point(0, 0);
+            this.list_txtFiles.Name = "list_txtFiles";
+            this.list_txtFiles.Size = new System.Drawing.Size(232, 339);
+            this.list_txtFiles.Sorted = true;
+            this.list_txtFiles.TabIndex = 7;
+            this.list_txtFiles.SelectedIndexChanged += new System.EventHandler(this.list_txtFiles_SelectedIndexChanged);
+            // 
+            // pic_open
+            // 
+            this.pic_open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic_open.BackgroundImage = global::MacroScript.Properties.Resources.folderimg;
+            this.pic_open.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pic_open.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_open.Location = new System.Drawing.Point(451, 2);
+            this.pic_open.Name = "pic_open";
+            this.pic_open.Size = new System.Drawing.Size(28, 18);
+            this.pic_open.TabIndex = 13;
+            this.pic_open.TabStop = false;
+            this.pic_open.Click += new System.EventHandler(this.pic_open_Click);
+            // 
+            // pic_PlayButton
+            // 
+            this.pic_PlayButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic_PlayButton.BackgroundImage = global::MacroScript.Properties.Resources.playbutton;
+            this.pic_PlayButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pic_PlayButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_PlayButton.Location = new System.Drawing.Point(451, 183);
+            this.pic_PlayButton.Name = "pic_PlayButton";
+            this.pic_PlayButton.Size = new System.Drawing.Size(30, 21);
+            this.pic_PlayButton.TabIndex = 12;
+            this.pic_PlayButton.TabStop = false;
+            this.pic_PlayButton.Click += new System.EventHandler(this.pic_PlayButton_Click);
+            // 
+            // pic_reload
+            // 
+            this.pic_reload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic_reload.BackgroundImage = global::MacroScript.Properties.Resources.macros_512;
+            this.pic_reload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pic_reload.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_reload.Location = new System.Drawing.Point(451, 28);
+            this.pic_reload.Name = "pic_reload";
+            this.pic_reload.Size = new System.Drawing.Size(28, 21);
+            this.pic_reload.TabIndex = 11;
+            this.pic_reload.TabStop = false;
+            this.pic_reload.Click += new System.EventHandler(this.pic_Reload_Click);
+            // 
+            // DropDown_Process
+            // 
+            this.DropDown_Process.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DropDown_Process.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.DropDown_Process.BeforeTouchSize = new System.Drawing.Size(206, 21);
+            this.DropDown_Process.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(218)))), ((int)(((byte)(218)))));
+            this.DropDown_Process.Location = new System.Drawing.Point(239, 28);
+            this.DropDown_Process.Name = "DropDown_Process";
+            this.DropDown_Process.Size = new System.Drawing.Size(206, 21);
+            this.DropDown_Process.Sorted = true;
+            this.DropDown_Process.Style = Syncfusion.Windows.Forms.VisualStyle.Office2016Black;
+            this.DropDown_Process.TabIndex = 14;
+            this.DropDown_Process.Text = "{Input Process Name}";
+            this.DropDown_Process.ThemeName = "Office2016Black";
+            this.DropDown_Process.DropDown += new System.EventHandler(this.DropDown_Process_Click);
+            this.DropDown_Process.TextChanged += new System.EventHandler(this.Drodown_Process_First_TextChange);
+            // 
+            // btn_GetFileDir
+            // 
+            this.btn_GetFileDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_GetFileDir.Location = new System.Drawing.Point(449, 210);
+            this.btn_GetFileDir.Name = "btn_GetFileDir";
+            this.btn_GetFileDir.Size = new System.Drawing.Size(32, 28);
+            this.btn_GetFileDir.TabIndex = 15;
+            this.btn_GetFileDir.Text = "FW";
+            this.btn_GetFileDir.UseVisualStyleBackColor = true;
+            this.btn_GetFileDir.Click += new System.EventHandler(this.btn_GetFileDir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 339);
+            this.Controls.Add(this.btn_GetFileDir);
+            this.Controls.Add(this.DropDown_Process);
+            this.Controls.Add(this.pic_open);
+            this.Controls.Add(this.pic_PlayButton);
+            this.Controls.Add(this.pic_reload);
+            this.Controls.Add(this.list_txtFiles);
+            this.Controls.Add(this.txt_DirFiles);
+            this.Controls.Add(this.txt_readfiles);
+            this.Controls.Add(this.Panel_readfile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MacroScript";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_open)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_PlayButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_reload)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DropDown_Process)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel Panel_readfile;
+        private System.Windows.Forms.TextBox txt_DirFiles;
+        private System.Windows.Forms.ListBox list_txtFiles;
+        private System.Windows.Forms.PictureBox pic_reload;
+        private System.Windows.Forms.PictureBox pic_PlayButton;
+        private System.Windows.Forms.PictureBox pic_open;
+        private Syncfusion.Windows.Forms.Tools.ComboBoxAdv DropDown_Process;
+        private System.Windows.Forms.Button btn_GetFileDir;
+        public System.Windows.Forms.TextBox txt_readfiles;
+    }
+}
+
